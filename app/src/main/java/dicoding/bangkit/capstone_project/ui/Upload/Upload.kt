23cf3fc39c61fac6abbdadb6081a99ea0c7d5a00
tria0987 <triaanggraini@student.uir.ac.id>
2024-06-13 11:03:14 +0700

    penambahan fungsi camera dan galery

--- app/src/main/java/dicoding/bangkit/capstone_project/ui/Upload/Upload.kt
+++ app/src/main/java/dicoding/bangkit/capstone_project/ui/Upload/Upload.kt
@@ -1,20 +1,73 @@
 package dicoding.bangkit.capstone_project.ui.Upload
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.WindowInsets
 import android.view.WindowManager
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import dicoding.bangkit.capstone_project.Helper.getImageUri
 import dicoding.bangkit.capstone_project.databinding.KlasifikasiActifityBinding
+import dicoding.bangkit.capstone_project.ui.homepage.Homepage
 
 class Upload : AppCompatActivity() {
 
     private lateinit var binding : KlasifikasiActifityBinding
+    private var currentImageUri : Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = KlasifikasiActifityBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setupView()
+
+        binding.buttonGalery.setOnClickListener{Startgalery() }
+        binding.buttonCamera.setOnClickListener{Startcamera()}
+        binding.backbutton.setOnClickListener {
+            startActivity(Intent(this@Upload , Homepage::class.java))
+        }
+    }
+
+
+    private fun Startgalery(){
+        launcherGallery.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+    }
+
+    private val launcherGallery = registerForActivityResult(
+        ActivityResultContracts.PickVisualMedia()
+    ) { uri: Uri? ->
+        if (uri != null) {
+            currentImageUri = uri
+            showImage()
+        } else {
+            Log.d("Photo Picker", "No media selected")
+        }
+    }
+
+    private fun Startcamera(){
+        currentImageUri = getImageUri(this)
+        launcherIntentCamera.launch(currentImageUri)
+    }
+
+    private val launcherIntentCamera = registerForActivityResult(
+        ActivityResultContracts.TakePicture()
+    ){ isSuccess ->
+        if (isSuccess) {
+            showImage()
+        }
+    }
+
+
+
+    private fun showImage() {
+        currentImageUri?.let {
+            Log.d("Image URI", "showImage: $it")
+            binding.buttonGalery.setImageURI(it)
+        }
     }
 
     private fun setupView() {
